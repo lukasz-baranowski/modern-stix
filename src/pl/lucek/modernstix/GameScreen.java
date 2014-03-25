03ff5135b6fa2a61b124231123f6196167e7c2b3
lucek <lucek@lucek.(none)>
2014-03-25 20:34:42 +0100

    initial commit

---
+++ src/pl/lucek/modernstix/GameScreen.java
@@ -0,0 +1,55 @@
+package pl.lucek.modernstix;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+	private Game game;
+
+	public GameScreen(Game game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/pl/lucek/modernstix/ModernStix.java
@@ -0,0 +1,27 @@
+package pl.lucek.modernstix;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class ModernStix extends Game {
+	public static final int FRAME_WIDTH = 1280;
+	public static final int FRAME_HEIGHT = 900;
+
+	@Override
+	public void create() {
+		setScreen(new GameScreen(this));
+	}
+	
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration lwjglApplicationConfiguration = new LwjglApplicationConfiguration();
+		lwjglApplicationConfiguration.fullscreen = false;
+		lwjglApplicationConfiguration.width = FRAME_WIDTH;
+		lwjglApplicationConfiguration.height = FRAME_HEIGHT;
+//		lwjglApplicationConfiguration.useCPUSynch = false;
+		lwjglApplicationConfiguration.vSyncEnabled = false;
+		lwjglApplicationConfiguration.title = "Modern Stix";
+		new LwjglApplication(new ModernStix(), lwjglApplicationConfiguration);
+	}
+
+}
