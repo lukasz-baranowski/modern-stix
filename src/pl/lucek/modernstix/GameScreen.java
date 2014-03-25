a38a85db45e6738fa3937a925e802b09cf13f035
lucek <lucek@lucek.(none)>
2014-03-25 21:53:03 +0100

    add junit

--- src/pl/lucek/modernstix/GameScreen.java
+++ src/pl/lucek/modernstix/GameScreen.java
@@ -1,55 +1,67 @@
 package pl.lucek.modernstix;
 
+import com.artemis.World;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameScreen implements Screen {
 	private Game game;
+	private World world;
+	private OrthographicCamera camera;
 
 	public GameScreen(Game game) {
 		this.game = game;
-	}
-	
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
+		this.camera = new OrthographicCamera(ModernStix.FRAME_WIDTH, ModernStix.FRAME_HEIGHT);
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
+		world = new World();
+		world.initialize();
 	}
 
 	@Override
 	public void render(float arg0) {
-		// TODO Auto-generated method stub
+		Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		camera.update();
+
+		world.setDelta(arg0);
+		if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			for(int i = 0; 10 > i; i++) {
+				world.process();
+			}
+		}
+		world.process();
+		
+//		spriteRenderSystem.process();
+//		healthRenderSystem.process();
+//		hudRenderSystem.process();
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
 	}
 
 	@Override
 	public void resize(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
---
+++ test/pl/lucek/modernstix/test/TestApp.java
@@ -0,0 +1,12 @@
+package pl.lucek.modernstix.test;
+
+import org.junit.Test;
+
+public class TestApp {
+
+	@Test
+	public void testJUnit() {
+		assert(true);
+	}
+
+}
